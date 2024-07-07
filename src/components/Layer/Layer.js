2c4b0ee22afbef4a3aaba779b7023b8f9752567d
kevindirkmunroe <kevin.munroe@gmail.com>
2024-07-07 03:16:19 -0700

    skeleton for edit pattern and color

--- src/components/Layer/Layer.js
+++ src/components/Layer/Layer.js
@@ -19,6 +19,14 @@ function Layer({level, patternName, patternImageKey, backgroundColor, patternOpa
     onDeleteLayer(level);
   }
 
+  const onEditPattern = () => {
+    console.log(`Layer: edit PATTERN level: ${level} pattern: ${patternName} opacity: ${patternOpacity}`);
+  }
+
+  const onEditColor = () => {
+    console.log(`Layer: edit COLOR level: ${level} color: ${backgroundColor} metallic: ${isColorMetallic}`);
+  }
+
   return (
     <View style={{flex: 1, flexDirection: 'row', flexGrow: 0.5, alignItems: 'center'}}>
       <View
@@ -32,13 +40,24 @@ function Layer({level, patternName, patternImageKey, backgroundColor, patternOpa
           <View style={{flex: 2, margin: 2, backgroundColor: '#f1f1f1', alignItems: 'center', justifyContent: 'center'}}><Text style={{margin: 10}}>{level}</Text>
           </View>
           <View style={{flex: 4, margin: 2,flexDirection: 'row' , alignItems: 'center', justifyContent: 'center', backgroundColor: '#f1f1f1'}}>
-            <Image style={{width: 30, height: 30, marginRight: 5, marginTop: 10}} source={staticImageUrlMap[patternImageKey]}></Image>
-            <Text>{patternName}</Text>
-            <Text> / {typeof patternOpacity === 'number'? patternOpacity : ''}%</Text>
+            <TouchableHighlight
+               onPress={onEditPattern}>
+              <View style={{flex: 1, flexDirection: 'row', alignContent: 'center', marginTop: 7}}>
+                <Image style={{width: 30, height: 30, marginRight: 5, marginTop: 10}} source={staticImageUrlMap[patternImageKey]}></Image>
+                <Text>{patternName}</Text>
+                <Text> / {typeof patternOpacity === 'number'? patternOpacity : ''}%</Text>
+              </View>
+            </TouchableHighlight>
           </View>
           <View style={{flex: 4, margin: 2,flexDirection: 'row', backgroundColor: '#f1f1f1', alignItems: 'center', justifyContent: 'center'}}>
-            <View style={{width: 30, height: 30, marginRight: 5, marginTop: 10, backgroundColor: backgroundColor}}></View>
-            <Text>{backgroundColor} {JSON.stringify(isColorMetallic) === 'true' ? 'Metallic': ''}</Text>
+            <TouchableHighlight
+              disabled={backgroundColor === null}
+              onPress={onEditColor}>
+              <View style={{flex: 1, flexDirection: 'row', alignContent: 'center', marginTop: 7}}>
+                <View style={{width: 30, height: 30, marginRight: 5, marginTop: 10, backgroundColor: backgroundColor}}></View>
+                <Text>{backgroundColor} {JSON.stringify(isColorMetallic) === 'true' ? 'Metallic': ''}</Text>
+              </View>
+            </TouchableHighlight>
           </View>
           <View style={{flex: 1, margin: 2,backgroundColor: '#f1f1f1', alignItems: 'center', justifyContent: 'center'}}>
             { level !== 'Background' && <TouchableHighlight
